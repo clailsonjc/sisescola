e04d7da9f02ef212f9b711e86b0530d100a0126b
Clailson <clailsonjc@gmail.com>
2018-09-08 11:33:21 -0300

    adicionado repositorio disciplina

---
+++ src/main/java/com/sisescola/controller/DisciplinaController.java
@@ -0,0 +1,33 @@
+package com.sisescola.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sisescola.Repository.DisciplinaRepository;
+import com.sisescola.models.Disciplina;
+
+@Controller
+@RequestMapping("/disciplinas")
+public class DisciplinaController {
+	
+	@Autowired
+	private DisciplinaRepository repositorioDisciplina;
+	
+	@GetMapping("/listar")
+	public ModelAndView listar() {
+		
+		ModelAndView resultado = new ModelAndView("/pages/blog");
+		List<Disciplina> listar = repositorioDisciplina.findAll();
+		resultado.addObject("listarDisciplinas", listar);
+		
+		return resultado;
+	}
+	
+	
+
+}//fim class
